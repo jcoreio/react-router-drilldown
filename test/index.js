caa90d6924aec206ada3be32e7dc2d4fd0c9caf3
Andy Edwards <jedwards@fastmail.com>
2017-09-09 21:08:55 -0500

    test(index): fix tests

--- test/index.js
+++ test/index.js
@@ -3,20 +3,20 @@
 import React from 'react'
 import {mount} from 'enzyme'
 import {expect} from 'chai'
-import {Router, Route, IndexRoute, Link, createMemoryHistory} from 'react-router'
-import Drilldown from '../src'
+import {createMemoryHistory} from 'history'
+import {Router, Route, Link} from 'react-router-dom'
+import Drilldown from '../src/withTransitionContext'
+import TransitionContext from 'react-transition-context'
 
 function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-describe('Drilldown', () => {
+describe('Drilldown', function () {
+  this.timeout(10000)
   let comp, history
 
   function init(location = '/') {
-    const TopDrilldown = props => <Drilldown {...props} viewportClassName="top-drilldown" />
-    const MidDrilldown = props => <Drilldown {...props} viewportClassName="mid-drilldown" />
-
     const Home = () => (
       <div className="home">
         <h1>Home</h1>
@@ -25,26 +25,33 @@ describe('Drilldown', () => {
       </div>
     )
 
-    const Users = () => (
+    const Users = ({match}) => (
       <div className="users">
         <h1>Users</h1>
-        <Link to="/users/andy" className="andy-link">Andy</Link>
+        <Link to={`${match.url}/andy`} className="andy-link">Andy</Link>
       </div>
     )
 
     const Andy = () => <div className="andy"><h1>Andy</h1></div>
 
-    history = createMemoryHistory(location)
+    const UsersRoute = ({match}) => (
+      <Drilldown viewportClassName="mid-drilldown">
+        <Route exact path={match.url} component={Users} />
+        <Route path={`${match.url}/andy`} component={Andy} />
+      </Drilldown>
+    )
+
+    history = createMemoryHistory({
+      initialEntries: [location],
+      initialIndex: 0,
+    })
 
     comp = mount(
       <Router history={history}>
-        <Route path="/" component={TopDrilldown}>
-          <IndexRoute component={Home} />
-          <Route path="users" component={MidDrilldown}>
-            <IndexRoute component={Users} />
-            <Route path="andy" component={Andy} />
-          </Route>
-        </Route>
+        <Drilldown viewportClassName="top-drilldown">
+          <Route exact path="/" component={Home} />
+          <Route path="/users" component={UsersRoute} />
+        </Drilldown>
       </Router>
     )
   }
@@ -56,51 +63,45 @@ describe('Drilldown', () => {
     init()
 
     expect(comp.find('h1').text()).to.equal('Home')
-    const home = comp.find('.top-drilldown').children().at(0)
-    expect(home.prop('data-transition-state')).to.equal('in')
+    expect(comp.find('.top-drilldown').find(TransitionContext).prop('transitionState')).to.equal('in')
     history.push('/users')
     await delay(300)
-    const usersParent = comp.find('.top-drilldown').children().at(1)
-    expect(home.prop('data-transition-state')).to.equal('leaving')
-    expect(usersParent.prop('data-transition-state')).to.equal('entering')
+    expect(comp.find('.top-drilldown').find(TransitionContext).at(1).prop('transitionState')).to.equal('entering')
+    expect(comp.find('.top-drilldown').find(TransitionContext).at(0).prop('transitionState')).to.equal('leaving')
     await delay(500)
-    expect(usersParent.prop('data-transition-state')).to.equal('in')
+    expect(comp.find('.top-drilldown').find(TransitionContext).at(1).prop('transitionState')).to.equal('in')
+    expect(comp.find('.mid-drilldown').find(TransitionContext).at(0).prop('transitionState')).to.equal('in')
 
     history.push('/users/andy')
     await delay(300)
-    const users = comp.find('.mid-drilldown').children().at(0)
-    const andy = comp.find('.mid-drilldown').children().at(1)
-    expect(usersParent.prop('data-transition-state')).to.equal('in')
-    expect(users.prop('data-transition-state')).to.equal('leaving')
-    expect(andy.prop('data-transition-state')).to.equal('entering')
+    // there is now only one TransitionContext directly under .top-drilldown
+    expect(comp.find('.top-drilldown').find(TransitionContext).at(0).prop('transitionState')).to.equal('in')
+    expect(comp.find('.mid-drilldown').find(TransitionContext).at(0).prop('transitionState')).to.equal('leaving')
+    expect(comp.find('.mid-drilldown').find(TransitionContext).at(1).prop('transitionState')).to.equal('entering')
     await delay(500)
-    expect(andy.prop('data-transition-state')).to.equal('in')
+    // there is now only one TransitionContext directly under .mid-drilldown
+    expect(comp.find('.mid-drilldown').find(TransitionContext).at(0).prop('transitionState')).to.equal('in')
   })
   it('animates transitions to index routes', async () => {
     init('/users/andy')
 
-    const usersParent = comp.find('.top-drilldown').children().at(1)
-    const andy = comp.find('.mid-drilldown').children().at(1)
-    expect(usersParent.prop('data-transition-state')).to.equal('in')
-    expect(andy.prop('data-transition-state')).to.equal('in')
+    expect(comp.find('.top-drilldown').find(TransitionContext).at(0).prop('transitionState')).to.equal('in')
+    expect(comp.find('.mid-drilldown').find(TransitionContext).at(0).prop('transitionState')).to.equal('in')
     history.push('/users')
     await delay(300)
-    const users = comp.find('.mid-drilldown').children().at(0)
-    expect(usersParent.prop('data-transition-state')).to.equal('in')
-    expect(users.prop('data-transition-state')).to.equal('entering')
-    expect(andy.prop('data-transition-state')).to.equal('leaving')
+    expect(comp.find('.top-drilldown').find(TransitionContext).at(0).prop('transitionState')).to.equal('in')
+    expect(comp.find('.mid-drilldown').find(TransitionContext).at(0).prop('transitionState')).to.equal('entering')
+    expect(comp.find('.mid-drilldown').find(TransitionContext).at(1).prop('transitionState')).to.equal('leaving')
     await delay(500)
-    expect(usersParent.prop('data-transition-state')).to.equal('in')
-    expect(users.prop('data-transition-state')).to.equal('in')
+    expect(comp.find('.top-drilldown').find(TransitionContext).at(0).prop('transitionState')).to.equal('in')
+    expect(comp.find('.mid-drilldown').find(TransitionContext).at(0).prop('transitionState')).to.equal('in')
 
     history.push('/')
     await delay(300)
-    const home = comp.find('.top-drilldown').children().at(0)
-    expect(home.prop('data-transition-state')).to.equal('entering')
-    expect(usersParent.prop('data-transition-state')).to.equal('leaving')
-    expect(users.prop('data-transition-state')).to.equal('in')
+    expect(comp.find('.top-drilldown').find(TransitionContext).at(0).prop('transitionState')).to.equal('entering')
+    expect(comp.find('.top-drilldown').find(TransitionContext).at(1).prop('transitionState')).to.equal('leaving')
     await delay(500)
-    expect(home.prop('data-transition-state')).to.equal('in')
+    expect(comp.find('.top-drilldown').find(TransitionContext).at(0).prop('transitionState')).to.equal('in')
   })
   it('handles quick transitions back and forth gracefully', async () => {
     init()
